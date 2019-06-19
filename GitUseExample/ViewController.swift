42f930977763b08126fbdc91c86bea8c2a87b760
Victor <kingpin11@infrawareglobal.com>
2019-06-19 14:47:03 +0900

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Victor K Lee on 19/06/2019.
+//  Copyright © 2019 Victor K Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
